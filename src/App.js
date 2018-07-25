b67a4622c66fdd2c4c6e1b4e87cd49d60225e9a4
Eric Jonese <ericnatejones@gmail.com>
2018-07-25 05:10:26 -0600

    a bit of clean up

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ class App extends Component {
   }
 
   removeLine = () => {
-    if (this.state.hasStarted){
       const arrayLength = this.state.codeArray.length
       let whichLine = Math.floor(Math.random(arrayLength) * arrayLength)
       while (this.state.deletedLines.includes(whichLine)){
@@ -40,29 +39,10 @@ class App extends Component {
       this.setState(prevState => {
         return {
           code:newCode,
-          deletedLines: [...prevState.deletedLines, whichLine]
-        }
-      })
-
-    } else {
-      const arrayLength = this.state.codeArray.length
-      const whichLine = Math.floor(Math.random(arrayLength) * arrayLength)
-      const oneLineRemoved = this.state.codeArray.map((line, i)=>{
-        if(i === whichLine){
-          return "// code goes here"
-        } else {
-          return line
-        }
-      })
-      const newCode = oneLineRemoved.join("\n")
-      this.setState(prevState => {
-        return {
-          code:newCode,
           deletedLines: [...prevState.deletedLines, whichLine],
           hasStarted: true
         }
       })
-    }
   }
 
   render() {
